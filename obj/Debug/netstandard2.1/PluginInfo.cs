dc176c2af119ecd8ad6ef130f38c0effcfa99c2d
Electrostats <Super_justy@hotmail.com>
2023-03-22 09:40:09 -0400

    removed gitignore files

--- obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace TootTally.Tooter
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "TootTally.Tooter";
-        public const string PLUGIN_NAME = "TootTally.Tooter";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
